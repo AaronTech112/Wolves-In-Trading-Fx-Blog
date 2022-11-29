c3393cae0d2a8cd4dfff7c579a6e72aef80af987
AaronTech112 <97069270+AaronTech112@users.noreply.github.com>
2022-11-29 14:55:30 +0100

    Update urls.py

--- wolvesblog/urls.py
+++ wolvesblog/urls.py
@@ -26,8 +26,7 @@ urlpatterns = [
 
 from django.conf import settings
 from django.conf.urls.static import static
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL , document_root = settings.MEDIA_ROOT)
     
 
 handler404 = 'blog.views.handling_404'
